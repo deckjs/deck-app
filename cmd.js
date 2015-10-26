f7213656829b0f6734fb94ba42d3df258ce7754c
Cristian Kiss <cristian.kiss@nearform.com>
2015-10-26 14:41:08 +0200

    windows 10 deck init temporary fix

--- cmd.js
+++ cmd.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
+var os = require('os').type()
+
 var electron = /electron/i.test(process.argv[0])
 if (electron) {
   require('./')(require('minimist')(process.argv.slice(2)))
 }
 
 if (!electron) {
+  if (os === 'Windows_NT' && process.argv[2] === 'init') {
+    require('./')(require('minimist')(process.argv.slice(2)))
+    return
+  }
   process.title = 'Deck'
   var spawn = require('child_process').spawn
   spawn(
